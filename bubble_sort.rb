e264d1fbf4144fb0688e39b3dd3a55ceb6d1bd8b
cbpolley <cbpolley@hotmail.co.uk>
2020-07-11 16:25:55 +0100

    upload bubble_sort

---
+++ bubble_sort.rb
@@ -0,0 +1,23 @@
+def bubble_sort(number_array)
+  i = 0
+  finish = true
+  k = 1
+
+  until i == number_array.length - k
+    if number_array[i] > number_array[i + 1]
+      temp_holder = number_array[i + 1]
+      number_array.delete_at(i + 1)
+      number_array.insert(i, temp_holder)
+      finish = false
+    end
+    i += 1
+    if i == number_array.length - k && finish == false
+      finish = true
+      k += 1
+      i = 0
+    end
+  end
+  puts number_array.join(', ')
+end
+
+bubble_sort([4,3,78,2,0,2])
